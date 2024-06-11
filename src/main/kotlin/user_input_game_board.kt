a3c00663801765fec02dafbc329a33a592857988
Ella_Q_Kotlin <baellap8@gmail.com>
2024-06-11 12:25:17 -0700

    Hyperskill project for Console Tic-Tac-Toe **changes to using CharArray for input and joinToString

--- src/main/kotlin/user_input_game_board.kt
+++ src/main/kotlin/user_input_game_board.kt
@@ -2,11 +2,14 @@ const val TOP_BOTTOM_BORDER = "---------"
 const val SIDE_BORDER_CHAR = "|"
 
 fun main() {
-    val userInput = readln()
+    val userInput = readln().toCharArray()
     println(TOP_BOTTOM_BORDER)
-    val (first, second, third) = userInput.chunked(3)
-    println("$SIDE_BORDER_CHAR ${first[0]} ${first[1]} ${first[2]} $SIDE_BORDER_CHAR")
-    println("$SIDE_BORDER_CHAR ${second[0]} ${second[1]} ${second[2]} $SIDE_BORDER_CHAR")
-    println("$SIDE_BORDER_CHAR ${third[0]} ${third[1]} ${third[2]} $SIDE_BORDER_CHAR")
+    val first = userInput.slice(0..2)
+    val second = userInput.slice(3..5)
+    val third = userInput.slice(6..8)
+
+    println("$SIDE_BORDER_CHAR ${first.joinToString(separator = " ")} $SIDE_BORDER_CHAR")
+    println("$SIDE_BORDER_CHAR ${second.joinToString(separator = " ")} $SIDE_BORDER_CHAR")
+    println("$SIDE_BORDER_CHAR ${third.joinToString(separator = " ")} $SIDE_BORDER_CHAR")
     println(TOP_BOTTOM_BORDER)
 }
