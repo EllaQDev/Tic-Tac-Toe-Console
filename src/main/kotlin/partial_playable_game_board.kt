af9788e76b467e1db4a992547aca4a6c9fc1a156
Ella_Q_Kotlin <baellap8@gmail.com>
2024-06-14 12:02:49 -0700

    ** modifies partial_playable_game_board.kt to be completely playable for a game of tic tac toe ** extracts gameState assessment from user_input_game_board.kt into fun assessGameState() ** adds gameplay while loop to wrap each move, checking gamestate after each move, break when WIN, LOSS, or DRAW reached

--- src/main/kotlin/partial_playable_game_board.kt
+++ src/main/kotlin/partial_playable_game_board.kt
@@ -1,3 +1,4 @@
+import kotlin.math.absoluteValue
 
 fun printBoard(first: List<Char>, second: List<Char>, third: List<Char>) {
     println(TOP_BOTTOM_BORDER)
@@ -6,44 +7,106 @@ fun printBoard(first: List<Char>, second: List<Char>, third: List<Char>) {
     println("$SIDE_BORDER_CHAR ${third.joinToString(separator = " ")} $SIDE_BORDER_CHAR")
     println(TOP_BOTTOM_BORDER)
 }
+fun assessGameState(gameBoard: List<MutableList<Char>>): GameState {
+    var winner_X = false
+    var winner_O = false
+    val first = gameBoard[0]
+    val second = gameBoard[1]
+    val third = gameBoard[2]
+    //matching columns
+    for (i in 0..2){
+        if (first[i] == second[i] && first[i] == third[i]) {
+            if (first[i] == 'X') winner_X = true
+            if (first[i] == 'O') winner_O = true
+        }
+    }
+    //matching rows
+    if (first.all { it == 'X'} || second.all { it == 'X'} || third.all { it == 'X' }) {
+        winner_X = true
+    }
+    if (first.all { it == 'O'} || second.all { it == 'O'} || third.all { it == 'O' }) {
+        winner_O = true
+    }
+    //checking diagonals (two possible)
+    if (first.first() == second[1] && first.first() == third[2] &&
+        (first.first() == 'X' || first.first() == 'O')) {
+        if (first.first() == 'X') winner_X = true
+        if (first.first() == 'O') winner_O = true
+    }
+    if (first.last() == second[1] && first.last() == third.first()) {
+        if (first.last() == 'X') winner_X = true
+        if (first.last() == 'O') winner_O = true
+    }
+
+    //checking if empty cells (when no winner found)
+    var unfinished = false
+    if (!winner_X && !winner_O) {
+        if (gameBoard.flatten().count { it == '_'} > 0) {
+            unfinished = true
+        }
+    }
+
+//    var countDivergence = false
+//    if ((userInput.count { it == 'X'} - userInput.count{ it == 'O'}).absoluteValue >= 2) {
+//        countDivergence = true
+//    }
+    val gameState = when {
+        winner_X -> GameState.X_WINS
+        winner_O -> GameState.O_WINS
+        unfinished -> GameState.GAME_UNFINISHED
+        else -> GameState.DRAW
+    }
+    return gameState
+}
 class OutOfBoundException(override val message: String): Exception(message)
 class IllegalMoveException(override val message: String): Exception(message)
 
 fun main() {
-    val userInput = readln().toCharArray()
-
+    // val userInput = readln().toCharArray()
+    // changed to empty grid to start
+    val userInput = "_________"
     val first = userInput.slice(0..2).toMutableList()
     val second = userInput.slice(3..5).toMutableList()
     val third = userInput.slice(6..8).toMutableList()
 
     val gameBoard = listOf(first, second, third)
-
+    var gameCondition = GameState.GAME_UNFINISHED
     printBoard(first,second, third)
 
-    //accept user input
-    while(true) {
+    var currPlayer = 'X'
+    while(gameCondition != GameState.O_WINS || gameCondition != GameState.X_WINS ||
+    gameCondition != GameState.DRAW) {
 
-    try {
-            val playCoords = readln().split(" ")
-            val row = playCoords[0].toInt()
-            val col = playCoords[1].toInt()
-            if (row < 1 || row > 3 || col < 1 || col > 3) throw OutOfBoundException("Coordinates should be from 1 to 3!")
-            val current = when (row) {
-                1 -> gameBoard[0][col - 1]
-                2 -> gameBoard[1][col - 1]
-                else -> gameBoard[2][col - 1]
+    //accept user input
+        while(true) {
+            try {
+                val playCoords = readln().split(" ")
+                val row = playCoords[0].toInt()
+                val col = playCoords[1].toInt()
+                if (row < 1 || row > 3 || col < 1 || col > 3) throw OutOfBoundException("Coordinates should be from 1 to 3!")
+                val current = when (row) {
+                    1 -> gameBoard[0][col - 1]
+                    2 -> gameBoard[1][col - 1]
+                    else -> gameBoard[2][col - 1]
+                }
+                if (current == 'X' || current == 'O') throw IllegalMoveException("This cell is occupied! Choose another one!") else {
+                    gameBoard[row - 1][col - 1] = currPlayer
+                    currPlayer = if (currPlayer == 'X') 'O' else 'X'
+                    printBoard(first, second, third)
+                    break
+                }
+            } catch (e: NumberFormatException) {
+                println("You should enter numbers!")
+            } catch (e: OutOfBoundException) {
+                println(e.message)
+            } catch (e: IllegalMoveException) {
+                println(e.message)
             }
-            if (current == 'X' || current == 'O') throw IllegalMoveException("This cell is occupied! Choose another one!") else {
-                gameBoard[row - 1][col - 1] = 'X'
-                printBoard(first, second, third)
-                break
-            }
-        } catch (e: NumberFormatException) {
-            println("You should enter numbers!")
-        } catch (e: OutOfBoundException) {
-            println(e.message)
-        } catch (e: IllegalMoveException) {
-            println(e.message)
+        }
+        gameCondition = assessGameState(gameBoard)
+        if(gameCondition != GameState.GAME_UNFINISHED) {
+            println(gameCondition.msg)
+            break
         }
     }
 }
